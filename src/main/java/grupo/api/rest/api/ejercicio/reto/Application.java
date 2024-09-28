d75cb75d1aff27bfe67a7c960f14431ecf2fa4d7
Melys02 <melys0919@gmail.com>
2024-09-27 23:04:19 -0500

    inicio de proyecto

---
+++ src/main/java/grupo/api/rest/api/ejercicio/reto/Application.java
@@ -0,0 +1,13 @@
+package grupo.api.rest.api.ejercicio.reto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
