1c3b5f6420a4754fbde0ae824dd6e561c03ad8a4
Melys02 <melys0919@gmail.com>
2024-09-27 23:14:10 -0500

    producto controller

--- src/main/java/grupo/api/rest/api/ejercicio/reto/cotroller/ProductoController.java
+++ src/main/java/grupo/api/rest/api/ejercicio/reto/cotroller/ProductoController.java
@@ -1,4 +1,23 @@
 package grupo.api.rest.api.ejercicio.reto.cotroller;
 
+import grupo.api.rest.api.ejercicio.reto.modelos.Producto;
+import grupo.api.rest.api.ejercicio.reto.service.ProductoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/productos")
 public class ProductoController {
+
+    @Autowired
+    private ProductoService productoService;
+
+    @GetMapping
+    public List<Producto> obtenerProductos() {
+        return productoService.obtenerProductos();
+    }
 }
