d9a961c079619204b2df367cf65ece4aae4e4b01
zchanway <146023170+zchanway@users.noreply.github.com>
2024-09-27 23:41:52 -0500

    controller terminado

--- src/main/java/grupo/api/rest/api/ejercicio/reto/cotroller/OrdenController.java
+++ src/main/java/grupo/api/rest/api/ejercicio/reto/cotroller/OrdenController.java
@@ -1,4 +1,34 @@
 package grupo.api.rest.api.ejercicio.reto.cotroller;
 
+import grupo.api.rest.api.ejercicio.reto.modelos.Orden;
+import grupo.api.rest.api.ejercicio.reto.service.OrdenService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/ordenes")
 public class OrdenController {
+
+    @Autowired
+    private OrdenService ordenService;
+
+    @GetMapping
+    public List<Orden> obtenerOrdenes() {
+        return ordenService.obtenerOrdenes();
+    }
+
+    @PostMapping
+    public ResponseEntity<Orden> agregarOrden(@RequestBody Orden orden) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(ordenService.agregarOrden(orden));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> eliminarOrden(@PathVariable Long id) {
+        ordenService.eliminarOrden(id);
+        return ResponseEntity.noContent().build();
+    }
 }
