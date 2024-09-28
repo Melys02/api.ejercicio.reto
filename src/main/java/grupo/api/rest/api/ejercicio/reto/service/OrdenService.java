4949be8d26289a6d485832595efdf8181ad00814
zchanway <146023170+zchanway@users.noreply.github.com>
2024-09-27 23:34:06 -0500

    orden servicio

--- src/main/java/grupo/api/rest/api/ejercicio/reto/service/OrdenService.java
+++ src/main/java/grupo/api/rest/api/ejercicio/reto/service/OrdenService.java
@@ -1,4 +1,27 @@
 package grupo.api.rest.api.ejercicio.reto.service;
 
+import grupo.api.rest.api.ejercicio.reto.modelos.Orden;
+import grupo.api.rest.api.ejercicio.reto.repository.OrdenRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class OrdenService {
+
+    @Autowired
+    private OrdenRepository ordenRepository;
+
+    public List<Orden> obtenerOrdenes() {
+        return ordenRepository.findAll();
+    }
+
+    public Orden agregarOrden(Orden orden) {
+        return ordenRepository.save(orden);
+    }
+
+    public void eliminarOrden(Long id) {
+        ordenRepository.deleteById(id);
+    }
 }
